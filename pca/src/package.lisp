cf90944a96342d763da35015dd7917e2ef748ca4
Mike Maul <mike.maul@gmail.com>
2014-06-06 22:24:56 -0400

    update pca

---
+++ pca/src/package.lisp
@@ -0,0 +1,22 @@
+(defpackage :clml.pca
+  (:nicknames :hjs.learn.pca)
+  (:use :cl
+        :hjs.util.meta
+        :hjs.util.matrix
+        :hjs.util.eigensystems
+        :hjs.util.vector
+        :hjs.learn.read-data
+        :statistics
+        :hjs.learn.vars)
+  (:nicknames :pca)
+  (:export 
+   #:princomp
+   #:princomp-projection
+   #:sub-princomp
+   #:kernel-princomp
+   #:make-face-estimator
+   #:face-estimate
+   #:components
+   #:contributions
+   #:loading-factors
+   ))
